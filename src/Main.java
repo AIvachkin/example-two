e759b2bc6854c69a778f5d51500358a0c6d3fc5b
Андрей <andrews.ru83@gmail.com>
2022-04-19 23:27:18 +0300

    Коммит 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // задание 1
+        byte a = 6;
+        short b = -15454;
+        int c = 245454766;
+        long d = 2125875212L;
+        float e = 2.78454f;
+        double f = -34.45127865324;
+        char g = 43;
+        boolean h = a > 8;
+        System.out.println (h);
+        System.out.println(g);
+
+        // задание 2
+        float boxerWeight1 = 78.2f;
+        float boxerWeight2 = 82.7f;
+        float summBoxWeigth = boxerWeight1 + boxerWeight2;
+        float diffBoxWeight = boxerWeight2 - boxerWeight1;
+        System.out.println("Общий вес бойцов " + summBoxWeigth + " кг");
+        System.out.println("Разница между весами бойцов" + diffBoxWeight + " кг");
+
     }
 }
