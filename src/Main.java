fe5663e648ae56bc5dfb501aba4986391a4743f3
Андрей <andrews.ru83@gmail.com>
2022-04-19 22:42:39 +0300

    Коммит 2.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte a = 6;
+        short b = -15454;
+        int c = 245454766;
+        long d = 2125875212L;
+        float e = 2.78454f;
+        double f = -34.45127865324;
+        char g = 43;
+        boolean seven = a < 7;
+        System.out.println (seven);
+        System.out.println(g);
     }
 }
