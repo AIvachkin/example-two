a345a1e5aa1945b389eb54df1385c25171af5526
Андрей <andrews.ru83@gmail.com>
2022-04-20 00:17:05 +0300

    Коммит 2.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // задание 3
+        byte banan = 5;
+        byte bananWeight = 80;
+        short milk = 200;
+        int milkWeight = milk/100*105;
+        byte icecream = 2;
+        byte icecreamWeight = 100;
+        byte egg = 4;
+        byte eggWeight = 70;
+        int breakfastWeight = banan * bananWeight + milkWeight + icecream * icecreamWeight + egg * eggWeight;
+        float breakfastKG = breakfastWeight/1000f;
+        System.out.println("Вес спорт-завтрака " + breakfastKG + " кг");
+
     }
 }
